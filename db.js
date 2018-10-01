dd02afbc00d15f4f5e9321769251057fece0e3d1
Leah Queen <leah836@gmail.com>
2018-10-01 08:57:37 -0700

    Added db, app.js files

---
+++ db.js
@@ -0,0 +1,19 @@
+"use strict";
+object.defineProperty(exports, "_esModule", { value: true});
+var connectionString = "mongodb://leah:leah123@ds139950.mlab.com:39950/leah";
+var mongodb = require("mongodb");
+var Database = (function () {
+    function Database() {
+    }
+    Database.connect = function () {
+        var_this = this;
+        return mongodb.MongoClient.connect(connectionString).then(function (db) {
+            console.log("sucess");
+            _this.db =db;
+        }).catch(function (err) {
+            console.log(err);
+        });
+    };
+    return Database;
+}());
+exports.default = Database;
